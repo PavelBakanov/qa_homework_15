3ae2a28c058baaba23092ddfba96734dc2f8a34a
PavelBakanov <maxconghunter@mail.ru>
2025-01-13 22:51:16 +0300

    minor changes

--- src/test/java/models/RequestModel.java
+++ src/test/java/models/RequestModel.java
@@ -6,5 +6,5 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Data
 public class RequestModel {
-    String email, password, name, job;
+    String email, password, name, job, updatedAt;
 }
