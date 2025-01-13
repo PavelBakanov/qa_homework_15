3cfbe7221e4c4be62b83ddd5bdf577507a2b5b6b
PavelBakanov <maxconghunter@mail.ru>
2025-01-13 23:52:10 +0300

    minor changes

--- src/test/java/models/RequestModel.java
+++ src/test/java/models/RequestModel.java
@@ -1,10 +1,10 @@
 package models;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RequestModel {
     String email, password, name, job;
 }
